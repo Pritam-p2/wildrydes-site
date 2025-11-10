f5c73aa20a7031ef66389651a6e2caa734219769
Pritam-p2 <132052073+Pritam-p2@users.noreply.github.com>
2025-11-10 13:15:39 +0530

    Update Cognito user pool IDs and API invoke URL

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_fMkul0Fbi', 
-        userPoolClientId: '539dfgrgg7onkeevjvisjb3v47', 
+        userPoolId: 'ap-south-1_IACKp3xye', 
+        userPoolClientId: '7voma8a32o2if63s4qf0kufqnf', 
         region: 'ap-south-1' 
     },
     api: {
-        invokeUrl: 'https://b8hr1whxef.execute-api.ap-south-1.amazonaws.com/prod',
+        invokeUrl: '',
     }
 };
