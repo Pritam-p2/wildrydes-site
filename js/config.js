569317073031be420a0dee4f797cc830114a8b09
Pritam-p2 <132052073+Pritam-p2@users.noreply.github.com>
2025-11-10 21:27:06 +0530

    Hide Cognito config details in config.js
    Comment out sensitive Cognito configuration details.

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_IACKp3xye', 
-        userPoolClientId: '7voma8a32o2if63s4qf0kufqnf', 
-        region: 'ap-south-1' 
+        // userPoolId: 'ap-south-1_IACKp3xye', 
+        // userPoolClientId: '7voma8a32o2if63s4qf0kufqnf', 
+        // region: 'ap-south-1' 
     },
     api: {
         invokeUrl: 'https://fctspyq7tg.execute-api.ap-south-1.amazonaws.com/dev',
