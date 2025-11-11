361216026c1321a2b042c976b05067d2dde8487c
Pritam-p2 <132052073+Pritam-p2@users.noreply.github.com>
2025-11-11 17:29:43 +0530

    Set API invokeUrl for production environment

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'ap-south-1' 
     },
     api: {
-        invokeUrl: '',
+        invokeUrl: 'https://2p3efmln5d.execute-api.ap-south-1.amazonaws.com/prod',
     }
 };
