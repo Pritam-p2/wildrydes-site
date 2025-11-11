dd63bf04fa26371b8e158b0f122a7f1f68a56ca2
Pritam-p2 <132052073+Pritam-p2@users.noreply.github.com>
2025-11-11 16:17:29 +0530

    Update Cognito configuration values

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        // userPoolId: 'ap-south-1_IACKp3xye', 
-        // userPoolClientId: '7voma8a32o2if63s4qf0kufqnf', 
-        // region: 'ap-south-1' 
+        userPoolId: 'ap-south-1_P1hdkbwSQ', 
+        userPoolClientId: '3qbbrlpeokha7dgvt2qbhe9nfj', 
+        region: 'ap-south-1' 
     },
     api: {
         invokeUrl: 'https://fctspyq7tg.execute-api.ap-south-1.amazonaws.com/dev',
