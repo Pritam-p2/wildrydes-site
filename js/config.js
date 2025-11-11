f933f0fe15e298bc058b3aacba5f9c8f345ddccb
Pritam-p2 <132052073+Pritam-p2@users.noreply.github.com>
2025-11-11 16:38:55 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'ap-south-1_P1hdkbwSQ', 
-        userPoolClientId: '3qbbrlpeokha7dgvt2qbhe9nfj', 
+        userPoolClientId: '2qk2m6cki08cj2ep7pik0ns3rb', 
         region: 'ap-south-1' 
     },
     api: {
